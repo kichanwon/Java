1f2b439ef94d92f65d031791e76a07e8fb2bb76d
kichanwon <wgichan@gmail.com>
2025-02-12 11:27:34 +0900

    commit

--- 01_variable/src/edu/kh/variable/ex1/PrintExample.java
+++ 01_variable/src/edu/kh/variable/ex1/PrintExample.java
@@ -9,6 +9,17 @@ public class PrintExample {
 		 * %A
 		 * %b
 		 */
-		System.out.println("");
+		int d = 1;
+		int o = 8;
+		float f = 0.1f;
+		char c = 'a';
+		boolean b = false;
+		String str = "Hello!";
+		System.out.printf("%5d\n",d);
+		System.out.printf("%5o\n",o);
+		System.out.printf("%.3f\n",f);
+		System.out.printf("%5c\n",c);
+		System.out.printf("%5s\n",str);
+		System.out.printf("%5b\n",b);
 	}
 }
