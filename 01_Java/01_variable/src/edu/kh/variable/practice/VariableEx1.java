2b0da82b3ee9dd8b9f15c1a33cb80fddcdb138e5
kichanwon <wgichan@gmail.com>
2025-02-12 11:15:04 +0900

    rebuild branch

--- 01_Java/01_variable/src/edu/kh/variable/practice/VariableEx1.java
+++
@@ -1,7 +0,0 @@
-package edu.kh.variable.practice;
-
-public class VariableEx1 {
-	public static void main(String[] args) {
-		System.out.println();
-	}
-}
---
+++ 01_variable/src/edu/kh/variable/ex1/PrintExample.java
@@ -0,0 +1,14 @@
+package edu.kh.variable.ex1;
+
+public class PrintExample {
+	public static void main(String[] args) {
+		/*
+		 * %d %o %x
+		 * %c %s
+		 * %f %e %g
+		 * %A
+		 * %b
+		 */
+		System.out.println("");
+	}
+}
